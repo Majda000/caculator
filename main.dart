7bc29d0e98a499b8d28d757f76f53604730f65b2
Majda000 <mahiiqueen11@gmail.com>
2024-07-12 09:15:57 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,144 +1,69 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Calculator App",
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: const HomePage(),
-    );
-  }
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
-  var num1 = 0, num2 = 0, sum = 0;
-  final TextEditingController t1 = TextEditingController(text: "0");
-  final TextEditingController t2 = TextEditingController(text: "0");
+  final String url = "https://swapi.dev/api/people"; // Corrected URL
+  late List data;
 
-  void doAddition() {
-    setState(() {
-      num1 = int.parse(t1.text);
-      num2 = int.parse(t2.text);
-      sum = num1 + num2;
-    });
+  @override
+  void initState() {
+    super.initState();
+    getJSONData();
   }
 
-  void doSubtraction() {
-    setState(() {
-      num1 = int.parse(t1.text);
-      num2 = int.parse(t2.text);
-      sum = num1 - num2;
-    });
-  }
+  Future<String> getJSONData() async {
+    var response = await http.get(
+      Uri.parse(url), // Corrected URL parsing
+      headers: {"Accept": "application/json"},
+    );
 
-  void doMultiplication() {
     setState(() {
-      num1 = int.parse(t1.text);
-      num2 = int.parse(t2.text);
-      sum = num1 * num2;
+      var convertDataToJson = json.decode(response.body);
+      data = convertDataToJson['results'];
     });
-  }
 
-  void doDivision() {
-    setState(() {
-      num1 = int.parse(t1.text);
-      num2 = int.parse(t2.text);
-      if (num2 != 0) {
-        sum = num1 ~/ num2; // Use integer division to avoid decimal points
-      } else {
-        sum = 0; // Handle division by zero
-      }
-    });
-  }
-
-  void doClear() {
-    setState(() {
-      t1.text = "0";
-      t2.text = "0";
-      sum = 0;
-    });
+    return "success";
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Calculator App"),
+        title: const Text("Retrieve JSON via HTTP Get"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(60.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Output: $sum",
-              style: const TextStyle(
-                fontSize: 20.0,
-                color: Colors.purple,
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: "Enter number 1",
-              ),
-              controller: t1,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: "Enter number 2",
-              ),
-              controller: t2,
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: ListView.builder(
+        // ignore: unnecessary_null_comparison
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  onPressed: doAddition,
-                  child: const Text("+"),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
-                  onPressed: doSubtraction,
-                  child: const Text("-"),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                  onPressed: doMultiplication,
-                  child: const Text("*"),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  onPressed: doDivision,
-                  child: const Text("/"),
+                Card(
+                  margin: const EdgeInsets.all(20.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(data[index]['name']),
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
-              onPressed: doClear,
-              child: const Text("Clear"),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
